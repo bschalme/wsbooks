7798af60f530d196036876a51502a46cc0eafc2c
Brian Schalme <bschalme@airspeed.ca>
2014-12-15 06:31:49 -0600

    Use the injected Logger instance rather than the static class.

--- grails-app/jobs/EndOfScheduleJob.groovy
+++ grails-app/jobs/EndOfScheduleJob.groovy
@@ -1,9 +1,7 @@
-import java.util.logging.Logger;
-
 class EndOfScheduleJob {
     static triggers = {}
 
     def execute() {
-        Logger.info("End of Job.")
+        log.info("End of Job.")
     }
 }
